f8424a8c956e82a043bffd64ed38b463065e03a6
katiebbugs <79028196+katiebbugs@users.noreply.github.com>
2021-06-25 17:33:47 -0600

    partials

--- server.js
+++ server.js
@@ -1,12 +1,13 @@
 const express = require('express');
 const routes = require('./controllers/');
 const sequelize = require('./config/connection');
+const helpers = require('./utils/helpers');
 
 const app = express();
 const PORT = process.env.PORT || 3001;
 
 const exphbs = require('express-handlebars');
-const hbs = exphbs.create({});
+const hbs = exphbs.create({ helpers });
 
 const session = require('express-session');
 
